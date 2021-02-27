3e6f25b823aa95de91740e6f76f7d810afac4341
Cryton Andrew <crytonadrew21@gmail.com>
2021-02-27 10:09:54 +0200

    Initial commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Container} from "react-bootstrap"
+
+const HomeScreen = () => {    
+    return (
+        <Container>
+        <h1>Hello World</h1>
+      </Container>
+    )
+}
+
+export default HomeScreen
