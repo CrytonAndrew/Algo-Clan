3b81af4e0eecf816e434ce81f48024b3d2dc2c67
Cryton Andrew <crytonadrew21@gmail.com>
2021-02-27 12:07:24 +0200

    Reset

--- src/components/PositionTracker.js
+++ src/components/PositionTracker.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, {useState, useCallback} from 'react'
 
 const PositionTracker = () => {
+    const width = 960
+    const height = 500
+
+    const initialMousePosition = {x: width/2, y: height/2}
+
+    const [mousePosition, setMousePosition] = useState(initialMousePosition)
+
+    const handleMouseMove = useCallback((event) => {
+        const {clientY, clientX} = event;
+        setMousePosition({x: clientX, y: clientY})
+        console.log(clientX, clientY)
+    }, [setMousePosition])
     return (
         <div>
-            
+            <svg width={width} height={height} onMouseMove={handleMouseMove}>
+                <circle 
+                    cx={mousePosition.x + 10}
+                    cy={mousePosition.y + 10}
+                    r={10}
+                />
+            </svg>
         </div>
     )
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,32 +1,13 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {Container} from "react-bootstrap"
-// import colors from "../data/NamesColors.csv"
-// import CSVReader from "react-csv-reader"
+
 
 const HomeScreen = () => {
-    const width = 960
-    const height = 500
-
-    const initialMousePosition = {x: width/2, y: height/2}
-
-    const [mousePosition, setMousePosition] = useState(initialMousePosition)
-
-    const handleMouseMove = (event) => {
-        const {clientY, clientX} = event;
-        setMousePosition({x: clientX, y: clientY})
-        console.log(clientX, clientY)
-    }
+   
 
     return (
         <Container>
-        <svg width={width} height={height} onMouseMove={handleMouseMove}>
-            <circle 
-                cx={mousePosition.x + 10}
-                cy={mousePosition.y + 10}
-                r={10}
-            />
-        </svg>
-        {/* <CSVReader onFileLoaded={(colors, fileInfo) => console.dir(colors, fileInfo)} /> */}
+
       </Container>
     )
 }
