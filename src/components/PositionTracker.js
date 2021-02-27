e741b4082a1e7b6db698839d71ea088379e6c869
Cryton Andrew <crytonadrew21@gmail.com>
2021-02-27 11:30:40 +0200

    Pointer tracker

---
+++ src/components/PositionTracker.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PositionTracker = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PositionTracker
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Container} from "react-bootstrap"
+// import colors from "../data/NamesColors.csv"
+// import CSVReader from "react-csv-reader"
 
-const HomeScreen = () => {    
+const HomeScreen = () => {
+    const width = 960
+    const height = 500
+
+    const initialMousePosition = {x: width/2, y: height/2}
+
+    const [mousePosition, setMousePosition] = useState(initialMousePosition)
+
+    const handleMouseMove = (event) => {
+        const {clientY, clientX} = event;
+        setMousePosition({x: clientX, y: clientY})
+        console.log(clientX, clientY)
+    }
+
     return (
         <Container>
-        <h1>Hello World</h1>
+        <svg width={width} height={height} onMouseMove={handleMouseMove}>
+            <circle 
+                cx={mousePosition.x + 10}
+                cy={mousePosition.y + 10}
+                r={10}
+            />
+        </svg>
+        {/* <CSVReader onFileLoaded={(colors, fileInfo) => console.dir(colors, fileInfo)} /> */}
       </Container>
     )
 }
